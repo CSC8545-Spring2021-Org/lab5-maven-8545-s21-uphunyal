d47c8ce1fac046254921fd8f6d95863d36318256
Utsav Phunyal <uphunyal@villanova.edu>
2021-04-18 16:01:02 -0700

    One statement per line

--- src/main/java/edu/studio/sample/ConsoleApp.java
+++ src/main/java/edu/studio/sample/ConsoleApp.java
@@ -4,7 +4,9 @@ public class ConsoleApp {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello Maven"); System.out.println("Bye Maven");
+        System.out.println("Hello Maven"); 
+        System.out.println("Bye Maven");
+        
         
     }
 
